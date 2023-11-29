595dcbae0f9790783ccdcea7322a0d5aaff3b15d
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2023-11-28 20:29:56 -0600

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='minimalml',
-    version='0.1.1',
+    version='0.1.2',
     author='Techno Absurdist',
     author_email='andere.emi@gmail.com',
     description='A python package for out-of-the-box ML solutions',
