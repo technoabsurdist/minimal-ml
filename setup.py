00214c754366fd2a9ea3aa357113765c72d56f3d
techno absurdist <92827957+technoabsurdist@users.noreply.github.com>
2023-11-28 20:28:54 -0600

    minimalml readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mltoolbox',
+    name='minimalml',
     version='0.1.1',
     author='Techno Absurdist',
     author_email='andere.emi@gmail.com',
